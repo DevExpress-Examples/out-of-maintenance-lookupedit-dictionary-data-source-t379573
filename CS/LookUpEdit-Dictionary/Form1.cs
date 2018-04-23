08086caebf94ee11d5207bf120696ac38c5302f1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/LookUpEdit-Dictionary/Form1.cs
@@ -0,0 +1,51 @@
+﻿using DevExpress.XtraEditors.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LookUpEdit_Dictionary {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+            gridControl1.DataSource = new List<Product> {
+                new Product(){ ProductName="Chang", CategoryID =  0  },
+                new Product(){ ProductName="Ipoh Coffee", CategoryID =  0  },
+                new Product(){ ProductName="Ravioli Angelo", CategoryID = 1  },
+                new Product(){ ProductName="Filo Mix", CategoryID = 1  },
+                new Product(){ ProductName="Tunnbröd", CategoryID  = 1  },
+                new Product(){ ProductName="Konbu", CategoryID  = 2  },
+                new Product(){ ProductName="Boston Crab Meat", CategoryID  = 2  }
+            };
+
+            Dictionary<int, string> Categories = new Dictionary<int, string>();
+            Categories.Add(0, "Beverages");
+            Categories.Add(1, "Grains");
+            Categories.Add(2, "Seafood");
+
+            RepositoryItemLookUpEdit riLookUp = new RepositoryItemLookUpEdit();
+            riLookUp.DataSource = Categories;
+            // Change the column caption.
+            riLookUp.PopulateColumns();
+            riLookUp.Columns["Value"].Caption = "Name";
+
+            gridView1.Columns["CategoryID"].ColumnEdit = riLookUp;
+        }
+    }
+
+    public class Product {
+        public int CategoryID { get; set; }
+        public string ProductName { get; set; }
+    }
+
+    
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# LookUpEdit - Dictionary<> data source
+
+
+This example shows how to set up an in-place lookup editor when its lookup data source is a Dictionary<<em>simpleDataType</em>, <em>simpleDataType</em>> object.<br><br>In the example, integer values of the CategoryID grid column are edited using an in-place LookUpEdit control (RepositoryItemLookUpEdit). The lookup data source is a Dictionary<int, string> object that associates category IDs with category names.
+
+<br/>
+
+
